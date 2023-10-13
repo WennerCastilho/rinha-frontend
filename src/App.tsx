c7db9dfc9fd0c4013cff8640d5997c588a09e67f
Wenner Castilho <wenner.fernandes@seidor.com.br>
2023-10-13 11:46:04 -0300

    Fix: Logic for rendering the loading screen or JSON preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { useState } from "react"
+import { ContainerSC } from "./AppStyles"
+import { Home } from "./components/Home"
+import { ViewerJson } from "./components/ViewerJson"
+
 export const App = () => {
+  const [viewJson, setViewJson] = useState(false)
   return (
-    <div>JSON Tree Viwer</div>
+    <ContainerSC>
+      {viewJson ?
+        <ViewerJson stateViewJson={[setViewJson]} /> :
+        <Home stateViewJson={[setViewJson]} />}
+    </ContainerSC>
   )
 }
