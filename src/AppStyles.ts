56862fc61573f759ac484058bd72a286667663ac
Wenner Castilho <wenner.fernandes@seidor.com.br>
2023-10-13 22:39:06 -0300

    Fix: Style

--- src/AppStyles.ts
+++ src/AppStyles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 export const ContainerSC = styled.div`
-    width: 100vw;
-    height: 100vh;
+  width: 100vw;
+  height: 100vh;
 
-    overflow-x: hidden;
+  overflow-x: hidden;
 `
